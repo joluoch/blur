be28d72729ac169e6e2edc59e963e6471bd94e6d
joluoch <jakindaodhiambo@gmail.com>
2019-07-01 16:34:35 +0300

    Added tab navigation

---
+++ app/src/main/java/strathcafe/strathcafe/com/PagerAdapter.java
@@ -0,0 +1,45 @@
+package strathcafe.strathcafe.com;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import strathcafe.strathcafe.com.fragments.Discount;
+import strathcafe.strathcafe.com.fragments.main_menu;
+import strathcafe.strathcafe.com.fragments.veges_option;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNoOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumberOfTabs)
+    {
+        super(fm);
+        this.mNoOfTabs = NumberOfTabs;
+    }
+    @Override
+    public Fragment getItem(int position) {
+
+        switch(position)
+        {
+
+            case 0:
+                main_menu tab1 = new main_menu();
+                return tab1;
+            case 1:
+                veges_option tab2 = new veges_option();
+                return  tab2;
+            case 2:
+                Discount tab3 = new Discount();
+                return  tab3;
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return mNoOfTabs;
+    }
+}
--- app/src/main/java/strathcafe/strathcafe/com/customer/MainActivity.java
+++ app/src/main/java/strathcafe/strathcafe/com/customer/MainActivity.java
@@ -1,15 +1,139 @@
 package strathcafe.strathcafe.com.customer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
+import strathcafe.strathcafe.com.PagerAdapter;
 import strathcafe.strathcafe.com.R;
+import strathcafe.strathcafe.com.fragments.Discount;
+import strathcafe.strathcafe.com.fragments.main_menu;
+import strathcafe.strathcafe.com.fragments.veges_option;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements main_menu.OnFragmentInteractionListener,veges_option.OnFragmentInteractionListener,Discount.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final strathcafe.strathcafe.com.PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
+/*        // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Setting ViewPager for each Tabs
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+    // Add Fragments to Tabs
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new main_menu(), "Menu");
+        adapter.addFragment(new veges_option(), "Vegeterian");
+        adapter.addFragment(new Discount(), "Discounts");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }*/
+
+    /*   TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Menu"));
+        tabLayout.addTab(tabLayout.newTab().setText("Veges"));
+        tabLayout.addTab(tabLayout.newTab().setText("Discounts"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+    }
+}*/
